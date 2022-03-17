c37a973c9679678ebd91a66f61680efc143f7f3b
Mostafa Kazemi <mkay1375@gmail.com>
2022-03-17 21:13:50 +0330

    first commit

---
+++ src/main/java/ir/mkay/wiseknight/WiseKnightApplication.java
@@ -0,0 +1,13 @@
+package ir.mkay.wiseknight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiseKnightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WiseKnightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/mkay/wiseknight/WiseKnightApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.mkay.wiseknight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiseKnightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
